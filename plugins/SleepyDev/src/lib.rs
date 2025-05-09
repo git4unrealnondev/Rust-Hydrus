96350e01feb1a7413437ad8ff0504d980e72f47e
git4unrealnondev <30579641+git4unrealnondev@users.noreply.github.com>
2025-05-08 20:51:27 -0500

    fixing sleepydev

--- plugins/SleepyDev/src/lib.rs
+++ plugins/SleepyDev/src/lib.rs
@@ -5,21 +5,20 @@ static PLUGIN_NAME: &str = "SleepyDev";
 static PLUGIN_DESCRIPTION: &str = "Just makes the plugin manager wait. Dev use only lol";
 
 #[no_mangle]
-pub fn return_info() -> sharedtypes::PluginInfo {
-    let callbackvec = vec![sharedtypes::PluginCallback::Start];
-    sharedtypes::PluginInfo {
-        name: PLUGIN_NAME.to_string(),
-        description: PLUGIN_DESCRIPTION.to_string(),
-        version: 1.00,
-        api_version: 1.00,
-        callbacks: callbackvec,
-        communication: Some(sharedtypes::PluginSharedData {
-            thread: sharedtypes::PluginThreadType::Spawn,
-            com_channel: Some(sharedtypes::PluginCommunicationChannel::Pipe(
-                "".to_string(),
-            )),
-        }),
-    }
+pub fn get_global_info() -> Vec<sharedtypes::GlobalPluginScraper> {
+    let mut main = sharedtypes::return_default_globalpluginparser();
+    main.name = PLUGIN_NAME.to_string();
+    main.version = 0;
+    main.storage_type = Some(sharedtypes::ScraperOrPlugin::Plugin(
+        sharedtypes::PluginInfo2 {
+            com_type: sharedtypes::PluginThreadType::Spawn,
+            com_channel: true,
+        },
+    ));
+    main.callbacks = vec![sharedtypes::GlobalCallbacks::Start];
+    let out = vec![main];
+
+    out
 }
 
 #[no_mangle]
